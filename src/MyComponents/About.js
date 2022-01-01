c6f498abbb4e1967a18c59e52ddf7e50487e7dc7
Aarushi Maurya <71892863+aarushimaurya@users.noreply.github.com>
2022-01-02 00:02:22 +0530

    Update About.js

--- src/MyComponents/About.js
+++ src/MyComponents/About.js
@@ -3,8 +3,8 @@ import React from 'react'
 export const About = () => {
     return (
         <div>
-            <h3>About The App</h3>
-            <p>A simple react app to list the tasks you need to do.</p>
+            
+            <p>  A simple react app to list the tasks you need to do.</p>
         </div>
     )
 }
